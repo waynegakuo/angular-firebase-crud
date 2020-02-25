cb40c8fca749f4da2880bcc9b27066a17799a6f9
waynegakuo <waynejotham@gmail.com>
2020-02-25 23:54:41 +0300

    Slight edits

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: 'AIzaSyBt5_AKCDt7OHuH6DDzfOjlJHM8iz_bU7c',
+    authDomain: 'fir-crudapp-a18e7.firebaseapp.com',
+    databaseURL: 'https://fir-crudapp-a18e7.firebaseio.com',
+    projectId: 'fir-crudapp-a18e7',
+    storageBucket: 'fir-crudapp-a18e7.appspot.com',
+    messagingSenderId: '804885787239',
+    appId: '1:804885787239:web:a899eaff42607698ee9ac2',
+    measurementId: 'G-74XNPXGTBX'
+  }
 };
